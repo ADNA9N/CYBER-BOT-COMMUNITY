4dee9d14d991022c1c86d80783e1d1ffd4dfedb3
Adnan mirza <adnaann05@gmail.com>
2025-08-10 21:27:55 +0300

    Update 0-MEME.js

--- Script/commands/0-MEME.js
+++ Script/commands/0-MEME.js
@@ -277,6 +277,6 @@ const fs = global.nodemodule["fs-extra"];
 "https://i.imgur.com/4nFOS0w.jpg" ,
 "https://i.imgur.com/tC2Sy8a.jpg" ,
      ];
-     var callback = () => api.sendMessage({body:`💝 𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠r 𝐒𝐡𝐚𝐧𝐤𝐚𝐫 𝐬𝐮𝐦𝐚𝐧 💝`,attachment: fs.createReadStream(__dirname + "/cache/AOV.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/AOV.jpg"));  
+     var callback = () => api.sendMessage({body:`💝 𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠r 𝐒𝐀𝐦𝐞𝐞𝐫 𝐊𝐡𝐀𝐚𝐧 💝`,attachment: fs.createReadStream(__dirname + "/cache/AOV.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/AOV.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/AOV.jpg")).on("close",() => callback());
    };
