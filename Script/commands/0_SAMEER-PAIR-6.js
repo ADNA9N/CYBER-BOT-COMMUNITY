9405ee1450e7ba633246c3d70a4400a3671397d1
Adnan mirza <adnaann05@gmail.com>
2025-08-12 18:00:43 +0300

    Update and rename 0_ARIF-PAIR-6.js to 0_SAMEER-PAIR-6.js

--- Script/commands/0_SAMEER-PAIR-6.js
+++ Script/commands/0_SAMEER-PAIR-6.js
@@ -19,7 +19,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'annu.png');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/WdirrtE.jpg", path);
+    if (!existsSync(path)) await downloadFile("https://i.postimg.cc/hPnXpS3n/WdirrtE.jpg", path);
 }
 
 async function makeImage({ one, two }) {
