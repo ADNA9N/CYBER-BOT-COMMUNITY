10c57e4d322e675682549d590aae5fdc181cc0fc
Adnan mirza <adnaann05@gmail.com>
2025-08-14 01:45:44 +0300

    Update 0_BESTDP😇.js

--- Script/commands/0_BESTDP😇.js
+++ Script/commands/0_BESTDP😇.js
@@ -80,6 +80,6 @@ const fs = global.nodemodule["fs-extra"];
 "https://i.postimg.cc/tCFR3RnR/download.jpg",
 "https://i.postimg.cc/XqchvPXW/girl-photo-hd-wallpaper-hidden-face-dp-mano-name-photodoozy.jpg"
   ];
-   var callback = () => api.sendMessage({body:`💝 𝐌𝐚𝐝𝐞 𝐁𝐲 𝙁𝘼𝙄𝙕 𝘼𝙉𝙎𝘼𝙍𝙄 💝 : ${link.length}`,attachment: fs.createReadStream(__dirname + "/cache/5.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.jpg"));	
+   var callback = () => api.sendMessage({body:`💝 𝐌𝐚𝐝𝐞 𝐁𝐲 𝐒𝐀𝐦𝐞𝐞𝐫 𝐊𝐡𝐀𝐚𝐧 💝 : ${link.length}`,attachment: fs.createReadStream(__dirname + "/cache/5.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.jpg"));	
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/5.jpg")).on("close",() => callback());
    };
